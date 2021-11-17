e60af183e27d95194ca0e206265ed0127df8b473
EC2 Default User <ec2-user@ip-172-31-32-48.ec2.internal>
2021-11-17 04:37:35 +0000

    [Update]date17

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+  end
+
+  def index
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction ,:profile_image_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
    root to: 'homes#top'
     resources :books, only: [:new, :create, :index, :show, :destroy]
 
-    resources :users, only: [:show]
+    resources :users, only: [:show,:index]
     get 'home/about' => "homes#index"
 end
